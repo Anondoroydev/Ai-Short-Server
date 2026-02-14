8b3904806ea87d8dce17b499aac5dc7ba1999eb2
Ajoy Sarkar (Anondo) <ajoysarker553@gmail.com>
2026-02-14 22:30:58 +0600

    schema added

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,17 +1,5 @@
-import { z } from 'zod';
 import 'dotenv/config';
-
-export const envSchema = z.object({
-  PORT: z.coerce.number().min(1000),
-  APP_URL: z
-    .string()
-    .optional()
-    .default(`http://localhost:${process.env.PORT}  `),
-  NODE_ENV: z
-    .union([z.literal('development'), z.literal('production')])
-    .default('development'),
-  DATABASE_URL: z.string(),
-});
+import { envSchema } from '../zosSchema/env.validation.ts';
 
 const envParser = envSchema.safeParse(process.env);
 
