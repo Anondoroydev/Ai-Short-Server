6db3ef0664eb782001f62f5e0c280b0313050c7f
Ajoy Sarkar <155249242+Anondoroydev@users.noreply.github.com>
2026-03-21 16:26:29 +0600

    login done

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
-import type { ConfigAndEnv } from '../types/config.ts';
-import type { Environment } from '../types/env';
-import { env } from './env.ts';
+import type { ConfigAndEnv } from '../types/config.js';
+import type { Environment } from '../types/env.js';
+import { env } from './env.js';
 
 // TODO: Add more env variables
 
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,36 @@
 import type { Request, Response } from 'express';
-import { loginService } from '../services/auth.services.ts';
+import {
+  loginService,
+  loginCallbackService,
+  oauth2Client,
+  google,
+} from '../services/auth.services.ts';
+
 async function loginController(req: Request, res: Response) {
   const url = await loginService();
   res.redirect(url);
 }
-export { loginController };
+
+async function loginCallbackController(req: Request, res: Response) {
+  try {
+    const code = req.query.code as string;
+    if (!code) {
+      return res.status(400).json({ error: 'Code is required' });
+    }
+
+    await loginCallbackService(code);
+
+    const oauth = google.oauth2({
+      auth: oauth2Client,
+      version: 'v2',
+    });
+
+    const userInfo = await oauth.userinfo.get();
+    return res.json(userInfo.data);
+  } catch (error) {
+    console.error('Login callback error:', error);
+    return res.status(500).json({ error: 'Authentication failed' });
+  }
+}
+
+export { loginController, loginCallbackController };
